9d408c8dae9ccb144db725db96a84a5824828cfa
zabdiel98 <zab.esc22@outlook.com>
2018-01-30 13:24:58 -0500

    First commit

---
+++ Actor.java
@@ -0,0 +1,27 @@
+
+public class Actor {
+String firstname, lastname;
+
+public Actor (String first, String last) {
+	this.firstname=first;
+	this.lastname=last;
+
+}
+
+public String getFirstname() {
+	return firstname;
+}
+
+public void setFirstname(String firstname) {
+	this.firstname = firstname;
+}
+
+public String getLastname() {
+	return lastname;
+}
+
+public void setLastname(String lastname) {
+	this.lastname = lastname;
+}
+
+}
---
+++ Actor_Push.java
@@ -0,0 +1,10 @@
+
+public class Actor_Push {
+public static void main(String[]args) {
+	Actor actor=new Actor("","Shwazeneggar");
+	
+	System.out.print(actor.firstname);
+	System.out.print(" " + actor.lastname);
+	
+}
+}
