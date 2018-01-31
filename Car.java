e7546f2b5cf75052376df5bb62d4b54b92d31455
zabdiel98 <zab.esc22@outlook.com>
2018-01-31 12:50:36 -0500

    First commit

---
+++ Car.java
@@ -0,0 +1,36 @@
+
+public class Car {
+	String brand;
+	double price;
+	int year;
+
+public Car(String brand2, double price2, int year2){
+	this.brand=brand2;
+	this.price=price2;
+	this.year=year2;
+	
+}
+
+public String getBrand() {
+	return brand;
+}
+
+public void setBrand(String brand) {
+	this.brand = brand;
+}
+
+public double getPrice() {
+	return price;
+}
+
+public void setPrice(double price) {
+	this.price = price;
+}
+
+public int getYear() {
+	return year;
+}
+
+public void setYear(int year) {
+	this.year = year;
+}}
